32f6d1e2f530c99e1783499ba2c9c191a90d637c
naby Mbaye <mouhamadnabymbaye@gmail.com>
2025-11-19 22:30:08 +0000

    modele entraine et genere

--- main.py
+++ main.py
@@ -4,9 +4,11 @@ import json, os
 port = int(os.environ.get("PORT", 8080))
 app = Flask(__name__)
 
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
+
 if __name__ == "__main__":
-    app.run(host='0.0.0.0',port=port)
+    app.run(host="0.0.0.0", static_url_path="/static", port=port)
